b35c6c2bc1a43d6f4f39600d343e3b9adca871a0
avih7531 <45245854+avih7531@users.noreply.github.com>
2019-02-21 13:37:12 -0500

    Update Photogate.ino

--- Photogate.ino
+++ Photogate.ino
@@ -1,48 +1,45 @@
-int led = 13;
-int photo = A0;
+int led = 13; // attach led to pin 13
+int photo = A0; // attach the photoresistor to analog pin 0
 
-float MPH;
-long time1;
-long afterTime;
-long currentTime;
-float cmms;
+float MPH; // makes an interger called MPH
+long time1; // makes an integer called time1
+long afterTime; // makes an integer called afterTime
+long currentTime; // makes an integer called currentTime
+float cmms; // makes an integer called cmms
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(led, OUTPUT);
-  pinMode(photo, INPUT);
-  Serial.begin(9600);
-  digitalWrite(led, HIGH);
+void setup() { // put your setup code here, to run once:
+  pinMode(led, OUTPUT); // makes the led an output
+  pinMode(photo, INPUT); // makes the photoresistor an input
+  Serial.begin(9600); // starts serial monitor at 9600 baud
+  digitalWrite(led, HIGH); // writes the led high
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0); // makes an integer called sensorValue equal to whatever A0 is reading
+  delay(1); // delays 1 milisecond
 
-  int sensorValue = analogRead(A0);
-  delay(1);
+  while (analogRead(A0) > 900) { // >500 when the gate is not blocked.
+    ; // ask Josh ¯\_(ツ)_/¯
+  }
+  currentTime = millis(); // makes the current time equal to the millis int
+  delay(50); // delays 50 miliseconds
 
- while (analogRead(A0) > 900) { //>500 when the gate is not blocked. 
-   ;//  
-}
-  currentTime = millis();
-  delay(50);
+  while (analogRead(A0) < 900) { // <500 when gate is blocked
+    ; // ¯\_(ツ)_/¯
+  }
 
-while (analogRead(A0) < 900) {
- ;//
-}
+  afterTime = millis(); // makes an int called afterTime = to millis
+  delay(50); // delays 50 miliseconds
 
-  afterTime = millis();
-  delay(50);
-  
-  time1 = afterTime - currentTime;
-  Serial.print("MS =");
-  Serial.println(time1);
-  cmms =  8.5/time1; 
-  MPH = cmms * 22.369;
-  Serial.print("MPH = ");
-  Serial.println(MPH);
-  delay(250);
-  time1 = 0;
+  time1 = afterTime - currentTime; // makes time 1 equal to afterTime-currentTime
+  Serial.print("MS = "); // serial prints "MS = "
+  Serial.print(time1); // serial prints the time1 integer
+  cmms =  8.5 / time1; // makes cmms equal to 8.5/time1
+  MPH = cmms * 22.369; // makes MPH equal to cmms * 22.369
+  Serial.println("MPH = "); // serial prints "MPH = "
+  Serial.print(MPH); // serial prints the MPH integer
+  delay(250); // delays 250 miliseconds
+  time1 = 0; // makes time equal to 0
 
 }
